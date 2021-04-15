47a41664c93b196b67c9451677776d2441bb750c
BlueNightMagic Mao <jerry.mao.au@gmail.com>
2021-04-15 16:43:57 +1000

    Update DBLinkObject.cpp

--- server/Server/DBLink/DBLinkObject.cpp
+++ server/Server/DBLink/DBLinkObject.cpp
@@ -17,7 +17,6 @@ namespace tomatodb
 
 	DBLinkObject::~DBLinkObject()
 	{
-		return;
 	}
 
 	VOID DBLinkObject::CleanUp()
@@ -29,7 +28,8 @@ namespace tomatodb
 	BOOL DBLinkObject::Init(const DatabaseOptions& dbOptions)
 	{
 		__ENTER_FUNCTION
-		string db_path_name = EnvFileAPI::GetPathName(tableName, dbOptions.LINK_DATABASE_FOLDER);
+		string db_name = tableName + "_" + linkedTableName;
+		string db_path_name = EnvFileAPI::GetPathName(db_name, dbOptions.LINK_DATABASE_FOLDER);
 		Status s = DB::Open(openOptions, db_path_name, &pDb);
 		if (!s.ok()) {
 			leveldb::DestroyDB(db_path_name, openOptions);
@@ -65,6 +65,21 @@ namespace tomatodb
 		if (!s.ok()) {
 			//m_pAdmin->CreateDatabase(pDbObj->database_name);
 			Log::SaveLog(SERVER_LOGFILE, "ERROR:  link database. Message: %s", s.ToString().c_str());
+			return FALSE;
+		}
+		return TRUE;
+		__LEAVE_FUNCTION
+		return FALSE;
+	}
+
+	BOOL DBLinkObject::InsertKeysIntoLinks(const DatabaseOptions& dbOptions)
+	{
+		__ENTER_FUNCTION
+			string db_path_name = EnvFileAPI::GetPathName(tableName, dbOptions.LINK_DATABASE_FOLDER);
+		Status s = DB::Open(createOptions, db_path_name, &pDb);
+		if (!s.ok()) {
+			leveldb::DestroyDB(db_path_name, createOptions);
+			Log::SaveLog(SERVER_LOGFILE, "ERROR: Create link database. Message: %s", s.ToString().c_str());
 			return FALSE;
 		}
 		return TRUE;
